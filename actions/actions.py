0ef4cb2152a96f878d062a0a2d3b859c6f0e8a55
dominic wagas <pmrhunee@gmail.com>
2022-03-17 22:50:09 +0800

    sample rasax

--- actions/actions.py
+++ actions/actions.py
@@ -1,109 +1,214 @@
-from typing import Text, List, Optional, Dict, Any
+import logging
+from typing import Dict, Text, Any, List
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher, Action
 from rasa_sdk.forms import FormValidationAction
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk import Tracker, Action
-from dotenv import load_dotenv
-import os
-import requests
-import json
-import uuid
-
-load_dotenv()
+from rasa_sdk.events import AllSlotsReset, SlotSet
+from actions.snow import SnowAPI
+import random
 
 
-def get_env_var(key):
-    env_var = os.getenv(key)
-    if env_var is None:
-        raise RuntimeError(f"Environment variable {key} was not found.")
-    return env_var
+logger = logging.getLogger(__name__)
+vers = "vers: 0.1.0, date: Apr 2, 2020"
+logger.debug(vers)
+
+snow = SnowAPI()
+localmode = snow.localmode
+logger.debug(f"Local mode: {snow.localmode}")
 
 
-airtable_api_key = get_env_var("AIRTABLE_API_KEY")
-base_id = get_env_var("BASE_ID")
-table_name = get_env_var("TABLE_NAME")
-
-
-def create_newsletter_record(email, frequency, notifications, can_ask_age, age):
-    request_url = f"https://api.airtable.com/v0/{base_id}/{table_name}"
-
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": f"Bearer {airtable_api_key}",
-    }
-
-    data = {
-        "fields": {
-            "Id": str(uuid.uuid4()),
-            "Email": email,
-            "Frequency": frequency,
-            "Notifications?": notifications,
-            "Can ask age?": can_ask_age,
-            "Age": age,
-        }
-    }
-
-    print(request_url)
-    print(headers)
-    print(json.dumps(data))
-
-    try:
-        response = requests.post(
-            request_url, headers=headers, data=json.dumps(data)
-        )
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        raise SystemExit(err)
-
-    print(f"Response status code: {response.status_code}")
-    return response
-
-
-class ValidateNewsletterForm(FormValidationAction):
+class ActionAskEmail(Action):
     def name(self) -> Text:
-        return "validate_newsletter_form"
+        return "action_ask_email"
 
-    async def required_slots(
-            self,
-            slots_mapped_in_domain: List[Text],
-            dispatcher: "CollectingDispatcher",
-            tracker: "Tracker",
-            domain: "DomainDict",
-    ) -> Optional[List[Text]]:
-        if not tracker.get_slot("can_ask_age"):
-            slots_mapped_in_domain.remove("age")
-
-        return slots_mapped_in_domain
-
-
-class SubmitNewsletterForm(Action):
-
-    def name(self) -> Text:
-        return "submit_newsletter_form"
-
-    async def run(
-            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
-        email = tracker.get_slot("email")
-        frequency = tracker.get_slot("frequency")
-        notifications = tracker.get_slot("notifications")
-        can_ask_age = tracker.get_slot("can_ask_age")
-        age = tracker.get_slot("age")
-
-        response = create_newsletter_record(email, frequency, notifications, can_ask_age, age)
-
-        dispatcher.utter_message("Thanks, your answers have been recorded!")
-
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        if tracker.get_slot("previous_email"):
+            dispatcher.utter_message(template=f"utter_ask_use_previous_email",)
+        else:
+            dispatcher.utter_message(template=f"utter_ask_email")
         return []
 
-# class ActionSayStuff(Action):
-#     def name(self) -> Text:
-#         return "action_say_stuff"
-#
-#     def run(self,
-#             dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#         dispatcher.utter_message("Stuff")
-#
-#         return []
+
+def _validate_email(
+    value: Text,
+    dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: Dict[Text, Any],
+) -> Dict[Text, Any]:
+    """Validate email is in ticket system."""
+    if not value:
+        return {"email": None, "previous_email": None}
+    elif isinstance(value, bool):
+        value = tracker.get_slot("previous_email")
+
+    if localmode:
+        return {"email": value}
+
+    results = snow.email_to_sysid(value)
+    caller_id = results.get("caller_id")
+
+    if caller_id:
+        return {"email": value, "caller_id": caller_id}
+    elif isinstance(caller_id, list):
+        dispatcher.utter_message(template="utter_no_email")
+        return {"email": None}
+    else:
+        dispatcher.utter_message(results.get("error"))
+        return {"email": None}
+
+
+class ValidateOpenIncidentForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_open_incident_form"
+
+    def validate_email(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate email is in ticket system."""
+        return _validate_email(value, dispatcher, tracker, domain)
+
+    def validate_priority(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate priority is a valid value."""
+
+        if value.lower() in snow.priority_db():
+            return {"priority": value}
+        else:
+            dispatcher.utter_message(template="utter_no_priority")
+            return {"priority": None}
+
+
+class ActionOpenIncident(Action):
+    def name(self) -> Text:
+        return "action_open_incident"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Create an incident and return details or
+        if localmode return incident details as if incident
+        was created
+        """
+
+        priority = tracker.get_slot("priority")
+        email = tracker.get_slot("email")
+        problem_description = tracker.get_slot("problem_description")
+        incident_title = tracker.get_slot("incident_title")
+        confirm = tracker.get_slot("confirm")
+        if not confirm:
+            dispatcher.utter_message(
+                template="utter_incident_creation_canceled"
+            )
+            return [AllSlotsReset(), SlotSet("previous_email", email)]
+
+        if localmode:
+            message = (
+                f"An incident with the following details would be opened "
+                f"if ServiceNow was connected:\n"
+                f"email: {email}\n"
+                f"problem description: {problem_description}\n"
+                f"title: {incident_title}\npriority: {priority}"
+            )
+        else:
+            snow_priority = snow.priority_db().get(priority)
+            response = snow.create_incident(
+                description=problem_description,
+                short_description=incident_title,
+                priority=snow_priority,
+                email=email,
+            )
+            incident_number = (
+                response.get("content", {}).get("result", {}).get("number")
+            )
+            if incident_number:
+                message = (
+                    f"Successfully opened up incident {incident_number} "
+                    f"for you. Someone will reach out soon."
+                )
+            else:
+                message = (
+                    f"Something went wrong while opening an incident for you. "
+                    f"{response.get('error')}"
+                )
+        dispatcher.utter_message(message)
+        return [AllSlotsReset(), SlotSet("previous_email", email)]
+
+
+class IncidentStatusForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_incident_status_form"
+
+    def validate_email(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate email is in ticket system."""
+        return _validate_email(value, dispatcher, tracker, domain)
+
+
+class ActionCheckIncidentStatus(Action):
+    def name(self) -> Text:
+        return "action_check_incident_status"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Look up all incidents associated with email address
+           and return status of each"""
+
+        email = tracker.get_slot("email")
+
+        incident_states = {
+            "New": "is currently awaiting triage",
+            "In Progress": "is currently in progress",
+            "On Hold": "has been put on hold",
+            "Closed": "has been closed",
+        }
+        if localmode:
+            status = random.choice(list(incident_states.values()))
+            message = (
+                f"Since ServiceNow isn't connected, I'm making this up!\n"
+                f"The most recent incident for {email} {status}"
+            )
+        else:
+            incidents_result = snow.retrieve_incidents(email)
+            incidents = incidents_result.get("incidents")
+            if incidents:
+                message = "\n".join(
+                    [
+                        f'Incident {i.get("number")}: '
+                        f'"{i.get("short_description")}", '
+                        f'opened on {i.get("opened_at")} '
+                        f'{incident_states.get(i.get("incident_state"))}'
+                        for i in incidents
+                    ]
+                )
+
+            else:
+                message = f"{incidents_result.get('error')}"
+
+        dispatcher.utter_message(message)
+        return [AllSlotsReset(), SlotSet("previous_email", email)]
--- webchat/README.md
+++
@@ -1,1 +0,0 @@
-Serve `index.html` on `http://127.0.0.1:8080/` by running `npx http-server .` from this folder.
